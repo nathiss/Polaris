721c2c33f1fd99582c81c0fe8f723840ccad923b
nathiss <kamil.jakub.rusin@gmail.com>
2021-07-29 22:06:18 +0200

    Add brand image

--- .polaris-gh-pages/src/components/Jumbotron.tsx
+++ .polaris-gh-pages/src/components/Jumbotron.tsx
@@ -1,11 +1,21 @@
+import { Box } from '@material-ui/core';
 import React from 'react';
+import LazyLoad from 'react-lazyload';
 import './Jumbotron.scss';
+
+import BrandImage from '../images/brand-512x512.png';
 
 class Jumbotron extends React.Component {
     render() {
         return (
             <header className="site-header">
-                <h1>Polaris</h1>
+                <Box display="flex" flexDirection="row" justifyContent="center" alignItems="center">
+                    <LazyLoad once>
+                        <img className="brand-image" src={BrandImage} alt="Brand" />
+                    </LazyLoad>
+                    <h1 className="disable-select">Polaris</h1>
+                </Box>
+
                 <small>
                     A vector graphics gallery. View on<span> </span>
                     <a href="https://github.com/nathiss/Polaris">GitHub</a>.
