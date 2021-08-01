44ed17fd4d7ad056fbceb8284d2eb3430c91e967
nathiss <kamil.jakub.rusin@gmail.com>
2021-08-01 12:06:45 +0200

    Change theme colors
    * Add GitHub profile link to Footer

--- .polaris-gh-pages/src/components/Footer.tsx
+++ .polaris-gh-pages/src/components/Footer.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import './Footer.scss';
 
+import config from '../config.json';
+
 class Footer extends React.Component {
     render() {
         return (
             <footer className="site-footer">
                 <hr />
-                <p>&copy; Kamil Rusin</p>
+                <p>
+                    &copy;
+                    <a href={config['GITHUB_PROFILE_URL']}>Kamil Rusin</a>
+                </p>
             </footer>
         );
     }
